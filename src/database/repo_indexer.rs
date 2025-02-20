9f55128a202508450d2390681a7967470a42426a
Zebreus <zebreus@zebre.us>
2025-02-20 14:50:56 +0100

    Refactor repo indexer

--- src/database/repo_indexer.rs
+++ src/database/repo_indexer.rs
@@ -1,7 +1,8 @@
 use crate::database::utils::unsafe_user_key_to_did;
-use anyhow::Context;
+use anyhow::{anyhow, Context};
+use async_channel::{Receiver, Sender};
 use index_repo::index_repo;
-use log::{debug, error, info, warn};
+use log::{error, info, warn};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::{collections::BTreeSet, sync::Arc};
@@ -9,8 +10,31 @@ use surrealdb::{engine::any::Any, Surreal};
 
 mod index_repo;
 
+#[derive(Deserialize)]
+struct BskyFollowRes {
+    #[serde(rename = "in")]
+    pub from: surrealdb::RecordId,
+    #[serde(rename = "out")]
+    pub to: surrealdb::RecordId,
+    pub id: surrealdb::RecordId,
+}
+
+#[derive(Debug)]
+pub struct SharedState {
+    rx: Receiver<String>,
+    db: Surreal<Any>,
+    http_client: Client,
+}
+
+/// Database struct for a repo indexing timestamp
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LastIndexedTimestamp {
+    pub time_us: u64,
+    pub time_dt: surrealdb::Datetime,
+    pub error: Option<String>,
+}
+
 pub async fn start_full_repo_indexer(db: Surreal<Any>, max_tasks: usize) -> anyhow::Result<()> {
-    let mut processed_dids: BTreeSet<String> = BTreeSet::new();
     let (tx, rx) = async_channel::bounded(10_000);
 
     let state = Arc::new(SharedState {
@@ -21,102 +45,35 @@ pub async fn start_full_repo_indexer(db: Surreal<Any>, max_tasks: usize) -> anyh
 
     info!(target: "indexer", "Spinning up {} handler tasks", max_tasks);
 
-    for i in 0..max_tasks {
+    for thread_id in 0..max_tasks {
         let state = state.clone();
         tokio::spawn(async move {
-            /*   thread::Builder::new()
-            .name(format!("Indexer Thread {}", i))
-            .spawn(move || {
-                Builder::new_current_thread()
-                    .enable_io()
-                    .enable_time()
-                    .build()
-                    .unwrap()
-                    .block_on(async { */
-            let res = task_handler(state, i as u64).await;
-            if let Err(e) = res {
-                error!(target: "indexer", "Handler thread {} failed: {:?}", i, e);
-            } else {
-                debug!(target: "indexer", "Handler thread {} exited", i);
-            }
+            let result = repo_fetcher_task(state).await;
+            let error = result
+                .and::<()>(Err(anyhow!("Handler thread should never exit")))
+                .unwrap_err();
+            error!(target: "indexer", "Handler thread {} failed: {:?}", thread_id, error);
         });
-        /* })
-        .context("Failed to spawn handler thread")?; */
     }
 
-    let mut anchor = "3juj4".to_string();
+    repo_discovery_task(state, tx).await.unwrap();
+
+    Ok(())
+}
+
+async fn repo_fetcher_task(state: Arc<SharedState>) -> anyhow::Result<()> {
     loop {
-        info!(target: "repo_indexer", "anchor {}", anchor);
+        let did = state.rx.recv().await.unwrap();
 
-        let mut res = state
-            .db
-            .query(format!(
-                "SELECT id,in,out FROM follow:{}.. LIMIT 500000;",
-                anchor
-            ))
-            .await?;
-        let likes_res: Vec<BskyFollowRes> = res.take(0)?;
+        let result = index_repo(&state, &did).await;
 
-        if likes_res.is_empty() {
-            tokio::time::sleep(std::time::Duration::from_millis(10000)).await;
-            continue;
-        }
+        if let Err(error) = result {
+            warn!(target: "indexer", "Failed to index repo {}: {}", did, error);
 
-        anchor = format!("{}", likes_res.last().unwrap().id.key());
+            let error_message = format!("{}", error);
+            if format!("{}", error) == "Failed to parse CAR file: early eof" {
+                // TODO: Document what this case does
 
-        let mut dids: BTreeSet<String> = BTreeSet::new();
-
-        for like in likes_res {
-            for record_id in vec![like.from, like.to] {
-                let did = unsafe_user_key_to_did(&format!("{}", record_id.key()));
-                if !processed_dids.contains(&did) {
-                    dids.insert(did.clone());
-                    processed_dids.insert(did);
-                }
-            }
-        }
-
-        for did in dids {
-            tx.send(did)
-                .await
-                .context("Failed to send message to handler thread")?;
-        }
-
-        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
-    }
-}
-
-#[derive(Deserialize)]
-struct BskyFollowRes {
-    #[serde(rename = "in")]
-    pub from: surrealdb::RecordId,
-    #[serde(rename = "out")]
-    pub to: surrealdb::RecordId,
-
-    pub id: surrealdb::RecordId,
-}
-
-#[derive(Debug)]
-pub struct SharedState {
-    rx: async_channel::Receiver<String>,
-    db: Surreal<Any>,
-    http_client: Client,
-}
-
-async fn task_handler(state: Arc<SharedState>, task_id: u64) -> anyhow::Result<()> {
-    tokio::time::sleep(std::time::Duration::from_millis(task_id * 42)).await;
-    // loop infinitely, handling repo index tasks
-    loop {
-        // get the next repo to be indexed
-        let did = {
-            let x = state.rx.recv().await;
-            x.unwrap()
-        };
-
-        let res = index_repo(&state, &did).await;
-        if let Err(e) = res {
-            let e_str = format!("{}", e);
-            if e_str == "Failed to parse CAR file: early eof" {
                 let did_key = crate::database::utils::did_to_key(did.as_str())?;
                 let timestamp_us = std::time::SystemTime::now()
                     .duration_since(std::time::UNIX_EPOCH)
@@ -128,24 +85,59 @@ async fn task_handler(state: Arc<SharedState>, task_id: u64) -> anyhow::Result<(
                     .content(LastIndexedTimestamp {
                         time_us: timestamp_us as u64,
                         time_dt: chrono::Utc::now().into(),
-                        error: Some(e_str),
+                        error: Some(error_message),
                     })
                     .await?;
-            } else {
-                warn!(target: "indexer", "Failed to index repo {}: {}", did, e);
             }
-            /* match e.try_into() {
-                iroh_car::Error::Parsing(e) => {}
-                _ => {}
-            } */
         }
     }
 }
 
-/// Database struct for a repo indexing timestamp
-#[derive(Debug, Serialize, Deserialize)]
-pub struct LastIndexedTimestamp {
-    pub time_us: u64,
-    pub time_dt: surrealdb::Datetime,
-    pub error: Option<String>,
+async fn repo_discovery_task(state: Arc<SharedState>, tx: Sender<String>) -> anyhow::Result<()> {
+    // An ID that was used before the earliest data we are interested in
+    const OLDEST_USEFUL_ANCHOR: &str = "3juj4";
+
+    let mut processed_dids: BTreeSet<String> = BTreeSet::new();
+    let mut anchor = OLDEST_USEFUL_ANCHOR.to_string();
+    loop {
+        info!(target: "indexer", "anchor {}", anchor);
+
+        let mut result = state
+            .db
+            .query(format!(
+                "SELECT id,in,out FROM follow:{}.. LIMIT 500000;",
+                anchor
+            ))
+            .await?;
+        let follows: Vec<BskyFollowRes> = result.take(0)?;
+
+        if follows.is_empty() {
+            tokio::time::sleep(std::time::Duration::from_millis(10000)).await;
+            continue;
+        }
+
+        anchor = format!("{}", follows.last().unwrap().id.key());
+
+        let mut dids: BTreeSet<String> = BTreeSet::new();
+
+        for follow in &follows {
+            for record_id in [&follow.from, &follow.to] {
+                let did = unsafe_user_key_to_did(&format!("{}", record_id.key()));
+                if processed_dids.contains(&did) {
+                    continue;
+                }
+                dids.insert(did.clone());
+                processed_dids.insert(did);
+            }
+        }
+
+        for did in dids {
+            tx.send(did)
+                .await
+                .context("Failed to send message to handler thread")?;
+        }
+
+        // TODO: Remove and add proper backpressure
+        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    }
 }
