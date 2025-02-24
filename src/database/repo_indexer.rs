5fed4d8a995689d0051b04060ada3c8933fe8e45
Zebreus <zebreus@zebre.us>
2025-02-24 17:31:21 +0100

    Convert the repo indexer to use a stream

--- src/database/repo_indexer.rs
+++ src/database/repo_indexer.rs
@@ -1,15 +1,13 @@
-use crate::database::utils::unsafe_user_key_to_did;
-use anyhow::{anyhow, Context};
-use async_channel::{Receiver, Sender};
-use index_repo::index_repo;
-use log::{error, info, warn};
+use futures::StreamExt;
+use index_repo::PipelineItem;
+use log::{error, info};
+use repo_stream::RepoStream;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::{collections::HashSet, sync::Arc, time::Duration};
 use surrealdb::{engine::any::Any, Surreal};
-use tokio::time::sleep;
 
 mod index_repo;
+mod repo_stream;
 
 #[derive(Deserialize)]
 struct BskyFollowRes {
@@ -20,13 +18,6 @@ struct BskyFollowRes {
     pub id: surrealdb::RecordId,
 }
 
-#[derive(Debug)]
-pub struct SharedState {
-    rx: Receiver<String>,
-    db: Surreal<Any>,
-    http_client: Client,
-}
-
 /// Database struct for a repo indexing timestamp
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LastIndexedTimestamp {
@@ -35,132 +26,136 @@ pub struct LastIndexedTimestamp {
     pub error: Option<String>,
 }
 
-/// Size of the queue of discovered repos
-const QUEUE_SIZE: usize = 200;
-/// How long to sleep, when the discovery queue is full
-const DISCOVERY_QUEUE_FULL_BACKOFF: Duration = Duration::from_millis(500);
-/// How long to sleep, when the we caught up with discovery
-const DISCOVERY_CAUGHT_UP_BACKOFF: Duration = Duration::from_millis(500);
 /// An ID that was used before the earliest data we are interested in
 const OLDEST_USEFUL_ANCHOR: &str = "3juj4";
-/// How long to sleep, when the queue is empty
-const FETCHER_BACKOFF: Duration = Duration::from_millis(500);
+/// The size of the buffer between each pipeline stage in elements
+const BUFFER_SIZE: usize = 200;
+
+pub async fn to_async(did: String) -> String {
+    println!("did: {}", did);
+    did
+}
 
 pub async fn start_full_repo_indexer(db: Surreal<Any>, max_tasks: usize) -> anyhow::Result<()> {
-    let (tx, rx) = async_channel::bounded(QUEUE_SIZE);
-    let state = Arc::new(SharedState {
-        rx,
-        db,
-        http_client: Client::new(),
-    });
+    let http_client = Client::new();
 
     info!(target: "indexer", "Spinning up {} handler tasks", max_tasks);
 
-    for thread_id in 0..max_tasks {
-        let state = state.clone();
-        tokio::spawn(async move {
-            let result = repo_fetcher_task(state).await;
-            let error = result
-                .and::<()>(Err(anyhow!("Handler thread should never exit")))
-                .unwrap_err();
-            error!(target: "indexer", "Handler thread {} failed: {:?}", thread_id, error);
-        });
-    }
-
-    repo_discovery_task(state, tx).await.unwrap();
-
-    Ok(())
-}
-
-async fn repo_fetcher_task(state: Arc<SharedState>) -> anyhow::Result<()> {
-    loop {
-        let Ok(did) = state.rx.try_recv() else {
-            sleep(FETCHER_BACKOFF).await;
-            continue;
-        };
-
-        let result = index_repo(&state, &did).await;
-
-        if let Err(error) = result {
-            warn!(target: "indexer", "Failed to index repo {}: {}", did, error);
-
-            let error_message = format!("{}", error);
-            if format!("{}", error) == "Failed to parse CAR file: early eof" {
-                // TODO: Document what this case does
-
-                let did_key = crate::database::utils::did_to_key(did.as_str())?;
-                let timestamp_us = std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH)
-                    .unwrap()
-                    .as_micros();
-                let _: Option<super::definitions::Record> = state
-                    .db
-                    .upsert(("li_did", did_key))
-                    .content(LastIndexedTimestamp {
-                        time_us: timestamp_us as u64,
-                        time_dt: chrono::Utc::now().into(),
-                        error: Some(error_message),
-                    })
-                    .await?;
+    RepoStream::new(OLDEST_USEFUL_ANCHOR.to_string(), &db)
+        .map(to_async)
+        .buffer_unordered(BUFFER_SIZE)
+        .map(|x| {
+            println!("gonna process {}", x);
+            x
+        })
+        .map(|did| {
+            let db = &db;
+            let http_client = &http_client;
+            let item = PipelineItem::new(db, http_client, did);
+            item
+        })
+        .map(|item| async { item.check_indexed().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
             }
-        }
-    }
-}
-
-async fn repo_discovery_task(state: Arc<SharedState>, tx: Sender<String>) -> anyhow::Result<()> {
-    // Max number of items in the queue
-    let queue_capacity = tx.capacity().unwrap_or(QUEUE_SIZE);
-    assert!(queue_capacity > 3);
-    // Start fetching when the queue is emptier than this
-    let fetch_threshold = (((queue_capacity / 3) * 2) - 1).max(1);
-    // How many items to fetch at once
-    let fetch_amount = (queue_capacity / 3).max(1);
-    // Start printing a warning when the queue is too empty
-    let warning_threshold = queue_capacity / 3;
-
-    let mut processed_dids: HashSet<String> = HashSet::new();
-    let mut anchor = OLDEST_USEFUL_ANCHOR.to_string();
-    loop {
-        if tx.len() > fetch_threshold {
-            sleep(DISCOVERY_QUEUE_FULL_BACKOFF).await;
-            continue;
-        }
-
-        info!(target: "indexer", "Discovering follows starting from {}", anchor);
-        let mut result = state
-            .db
-            // TODO: Fix the possible SQL injection
-            .query(format!(
-                "SELECT id,in,out FROM follow:{}.. LIMIT {};",
-                anchor, fetch_amount
-            ))
-            .await?;
-        let follows: Vec<BskyFollowRes> = result.take(0)?;
-
-        let Some(anchor_key) = follows.last().map(|follow| follow.id.key()) else {
-            sleep(DISCOVERY_CAUGHT_UP_BACKOFF).await;
-            continue;
-        };
-        anchor = format!("{}", anchor_key);
-
-        let processed_dids_before = processed_dids.len();
-        for follow in &follows {
-            for record_id in [&follow.from, &follow.to] {
-                let did = unsafe_user_key_to_did(&format!("{}", record_id.key()));
-                if processed_dids.contains(&did) {
-                    continue;
-                }
-                processed_dids.insert(did.clone());
-                tx.send(did)
-                    .await
-                    .context("Failed to send message to handler thread")?;
+            result.ok()
+        })
+        .map(|item| async { item.get_service().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
             }
-        }
+            result.ok()
+        })
+        .map(|item| async { item.download_repo().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
+            }
+            result.ok()
+        })
+        .map(|item| async { item.deserialize_repo().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
+            }
+            result.ok()
+        })
+        .map(|item| async { item.files_to_updates().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
+            }
+            result.ok()
+        })
+        .map(|item| async { item.apply_updates().await })
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(|result| async {
+            if let Err(error) = &result {
+                error!(target: "indexer", "Failed to index repo: {}", error);
+            }
+            result.ok()
+        })
+        .for_each(|x| async {
+            x.print_report().await;
+        })
+        .await;
 
-        // Warn if it looks like the queue size or the backoff were choosen incorrectly
-        let new_follows = processed_dids.len() - processed_dids_before;
-        if new_follows != 0 && follows.len() == fetch_amount && tx.len() < warning_threshold {
-            warn!(target: "indexer", "Queue is not getting filled up fast enough. Consider increasing the queue size or decreasing the backoff.");
-        }
-    }
+    panic!("Done, this should not happen");
+
+    // .map(|did| {
+    //     // let state = state.clone();
+    //     let db = &db;
+    //     let client = &client;
+    //     async move {
+    //         let result = index_repo(&db, &client, &did).await;
+    //         if let Err(error) = result {
+    //             warn!(target: "indexer", "Failed to index repo {}: {}", did, error);
+
+    //             let error_message = format!("{}", error);
+    //             if format!("{}", error) == "Failed to parse CAR file: early eof" {
+    //                 // TODO: Document what this case does
+
+    //                 let did_key = crate::database::utils::did_to_key(did.as_str()).unwrap();
+    //                 let timestamp_us = std::time::SystemTime::now()
+    //                     .duration_since(std::time::UNIX_EPOCH)
+    //                     .unwrap()
+    //                     .as_micros();
+    //                 let _: Option<super::definitions::Record> = db
+    //                     .upsert(("li_did", did_key))
+    //                     .content(LastIndexedTimestamp {
+    //                         time_us: timestamp_us as u64,
+    //                         time_dt: chrono::Utc::now().into(),
+    //                         error: Some(error_message),
+    //                     })
+    //                     .await
+    //                     .unwrap();
+    //             }
+    //         }
+    //     }
+    // })
+    // .buffer_unordered(200)
+    // .for_each(|x| async {
+    //     println!("finished stream");
+    // })
+    // .await;
+
+    // for thread_id in 0..max_tasks {
+    //     let state = state.clone();
+    //     tokio::spawn(async move {
+    //         let result = repo_fetcher_task(state).await;
+    //         let error = result
+    //             .and::<()>(Err(anyhow!("Handler thread should never exit")))
+    //             .unwrap_err();
+    //         error!(target: "indexer", "Handler thread {} failed: {:?}", thread_id, error);
+    //     });
+    // }
+
+    // repo_discovery_task(state, tx).await.unwrap();
 }
---
+++ src/database/repo_indexer/repo_stream_nofuture.rs
@@ -0,0 +1,146 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    future::{Future, IntoFuture},
+    task::Poll,
+};
+
+use futures::Stream;
+use log::info;
+use surrealdb::{engine::any::Any, Surreal};
+
+use crate::database::{repo_indexer::BskyFollowRes, utils::unsafe_user_key_to_did};
+
+pub struct RepoStream<'a> {
+    buffer: VecDeque<String>,
+    processed_dids: HashSet<String>,
+    anchor: String,
+    db: &'a Surreal<Any>,
+    db_future: Option<
+        std::pin::Pin<
+            Box<
+                dyn Future<Output = Result<surrealdb::Response, surrealdb::Error>>
+                    + Send
+                    + Sync
+                    + 'a,
+            >,
+        >,
+    >,
+}
+
+impl<'a> RepoStream<'a> {
+    pub fn new(anchor: String, db: &'a Surreal<Any>) -> Self {
+        return Self {
+            buffer: VecDeque::new(),
+            processed_dids: HashSet::new(),
+            anchor,
+            db,
+            db_future: None,
+        };
+    }
+}
+
+const FETCH_AMOUNT: usize = 100;
+
+// async fn get_repos_from(db: &Surreal<Any>, anchor: &str) -> Vec<String> {
+//     info!(target: "indexer", "Discovering follows starting from {}", anchor);
+//     let mut result = db
+//         // TODO: Fix the possible SQL injection
+//         .query(format!(
+//             "SELECT id,in,out FROM follow:{}.. LIMIT {};",
+//             anchor, FETCH_AMOUNT
+//         ));
+//     let follows: Vec<BskyFollowRes> = result.take(0)?;
+
+//     let Some(anchor_key) = follows.last().map(|follow| follow.id.key()) else {
+//         sleep(DISCOVERY_CAUGHT_UP_BACKOFF).await;
+//         continue;
+//     };
+// }
+
+impl<'a> Stream for RepoStream<'a> {
+    type Item = String;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        loop {
+            if let Some(next) = self.buffer.pop_front() {
+                return Poll::Ready(Some(next));
+            }
+
+            info!(target: "indexer", "Discovering follows starting from {}", self.anchor);
+            let db_future = if self.db_future.is_some() {
+                self.db_future.as_mut().unwrap()
+            } else {
+                let mut result = self
+                    .db
+                    // TODO: Fix the possible SQL injection
+                    .query(format!(
+                        "SELECT id,in,out FROM follow:{}.. LIMIT {};",
+                        self.anchor, FETCH_AMOUNT
+                    ));
+                // let mut future: std::pin::Pin<
+                //     Box<
+                //         dyn Future<Output = Result<surrealdb::Response, surrealdb::Error>>
+                //             + Send
+                //             + Sync
+                //             + 'a,
+                //     >,
+                // >
+                let mut future = result.into_future();
+                self.db_future = Some(future);
+                self.db_future.as_mut().unwrap()
+            };
+
+            let Poll::Ready(result) = Future::poll(db_future.as_mut(), cx) else {
+                return Poll::Pending;
+            };
+            self.db_future = None;
+
+            let mut result = result.unwrap();
+
+            // let mut result: surrealdb::method::Query<'_, Any> = self
+            //     .db
+            //     // TODO: Fix the possible SQL injection
+            //     .query(format!(
+            //         "SELECT id,in,out FROM follow:{}.. LIMIT {};",
+            //         self.anchor, FETCH_AMOUNT
+            //     ));
+            let follows: Vec<BskyFollowRes> = result.take(0).unwrap();
+
+            let Some(anchor_key) = follows.last().map(|follow| follow.id.key()) else {
+                // sleep(DISCOVERY_CAUGHT_UP_BACKOFF).await;
+                // continue;
+                // TODO: Sleep again
+                return Poll::Pending;
+            };
+            self.anchor = format!("{}", anchor_key);
+
+            for follow in &follows {
+                for record_id in [&follow.from, &follow.to] {
+                    let did = unsafe_user_key_to_did(&format!("{}", record_id.key()));
+                    if self.processed_dids.contains(&did) {
+                        continue;
+                    }
+                    self.processed_dids.insert(did.clone());
+                    self.buffer.push_back(did);
+                    // tx.send(did)
+                    //     .await
+                    //     .context("Failed to send message to handler thread")?;
+                }
+            }
+
+            if let Some(next) = self.buffer.pop_front() {
+                return Poll::Ready(Some(next));
+            }
+            return Poll::Pending;
+
+            // Warn if it looks like the queue size or the backoff were choosen incorrectly
+            // let new_follows = self.processed_dids.len() - processed_dids_before;
+            // if new_follows != 0 && follows.len() == fetch_amount && tx.len() < warning_threshold {
+            //     warn!(target: "indexer", "Queue is not getting filled up fast enough. Consider increasing the queue size or decreasing the backoff.");
+            // }
+        }
+    }
+}
