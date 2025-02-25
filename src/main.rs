77a3f730efbb663f898e8c2c08e9816bd67c6b85
Zebreus <zebreus@zebre.us>
2025-02-25 18:39:35 +0100

    Improve telemetry setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+#![feature(type_changing_struct_update)]
+
 use anyhow::Context;
 use config::Args;
 use database::repo_indexer::start_full_repo_indexer;
 use metrics_reporter::export_system_metrics;
+use opentelemetry::{global, trace::TracerProvider as _, KeyValue};
 use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
 use opentelemetry_otlp::{LogExporter, MetricExporter, SpanExporter};
+use opentelemetry_resource_detectors::{
+    HostResourceDetector, OsResourceDetector, ProcessResourceDetector,
+};
 use opentelemetry_sdk::{
-    logs::SdkLoggerProvider, metrics::SdkMeterProvider, trace::SdkTracerProvider, Resource,
+    logs::SdkLoggerProvider,
+    metrics::{PeriodicReader, SdkMeterProvider},
+    propagation::TraceContextPropagator,
+    resource::EnvResourceDetector,
+    trace::{RandomIdGenerator, Sampler, SdkTracerProvider},
+    Resource,
 };
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    LazyLock,
+use opentelemetry_semantic_conventions::{
+    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_NAME, SERVICE_VERSION},
+    resource::{HOST_NAME, OS_BUILD_ID, OS_DESCRIPTION, OS_NAME, OS_VERSION, SERVICE_INSTANCE_ID},
+    SCHEMA_URL,
 };
-use surrealdb::{engine::any::Any, Surreal};
-use tokio::runtime::Builder;
+use std::{
+    process::exit,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, LazyLock,
+    },
+};
+use surrealdb::{engine::any::Any, Surreal, Uuid};
+use tokio::{runtime::Builder, signal::ctrl_c, time::interval_at};
 use tokio_rustls::rustls::crypto::aws_lc_rs::default_provider;
-use tracing::{error, info};
-use tracing_subscriber::{prelude::*, EnvFilter};
+use tracing::{error, span};
+use tracing_subscriber::{prelude::*, EnvFilter, Registry};
 
 mod config;
 mod database;
@@ -27,24 +46,74 @@ mod websocket;
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 const RESOURCE: LazyLock<Resource> = LazyLock::new(|| {
+    let instance_id = Uuid::new_v4();
+
+    let mut attributes = vec![
+        KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+        KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+        KeyValue::new(SERVICE_INSTANCE_ID, instance_id.to_string()),
+        KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
+    ];
+
+    if let Ok(linux_sys_info) = sys_info::linux_os_release() {
+        if let Some(build_id) = linux_sys_info.build_id {
+            attributes.push(KeyValue::new(OS_BUILD_ID, build_id));
+        }
+        if let Some(pretty_name) = linux_sys_info.pretty_name {
+            attributes.push(KeyValue::new(OS_DESCRIPTION, pretty_name));
+        }
+        if let Some(name) = linux_sys_info.name {
+            attributes.push(KeyValue::new(OS_NAME, name));
+        }
+        if let Some(version_id) = linux_sys_info.version_id {
+            attributes.push(KeyValue::new(OS_VERSION, version_id));
+        }
+    } else {
+        if let Ok(os_version) = sys_info::os_release() {
+            attributes.push(KeyValue::new(OS_DESCRIPTION, os_version));
+        }
+        if let Ok(os_name) = sys_info::os_type() {
+            attributes.push(KeyValue::new(OS_NAME, os_name));
+        }
+    }
+
+    if let Ok(hostname) = sys_info::hostname() {
+        attributes.push(KeyValue::new(HOST_NAME, hostname));
+    }
+
     Resource::builder()
-        .with_service_name("rust-indexer")
+        .with_schema_url(
+            [
+                KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+                KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+                KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
+            ],
+            SCHEMA_URL,
+        )
+        .with_attributes(attributes)
+        .with_detectors(&[
+            Box::new(EnvResourceDetector::new()),
+            Box::new(HostResourceDetector::default()),
+            Box::new(ProcessResourceDetector),
+            Box::new(OsResourceDetector),
+            // Box::new(OsResourceDetector::new()),
+        ])
         .build()
 });
 
-fn init_logger() -> SdkLoggerProvider {
-    let otlp_log_exporter = LogExporter::builder().with_tonic().build().unwrap();
-    let logger_provider = SdkLoggerProvider::builder()
-        .with_resource(RESOURCE.clone())
-        .with_batch_exporter(otlp_log_exporter)
-        .build();
-    let otel_filter = EnvFilter::new("info")
+fn init_observability() -> OtelGuard {
+    let tracer_provider = init_tracer();
+    let meter_provider = init_meter();
+    let logger_provider = init_logger();
+
+    let otel_log_filter = EnvFilter::new("info")
         .add_directive("hyper=off".parse().unwrap())
         .add_directive("h2=off".parse().unwrap())
         .add_directive("opentelemetry=off".parse().unwrap())
         .add_directive("tonic=off".parse().unwrap())
         .add_directive("reqwest=off".parse().unwrap());
-    let otel_layer = OpenTelemetryTracingBridge::new(&logger_provider).with_filter(otel_filter);
+    let otel_log_layer =
+        OpenTelemetryTracingBridge::new(&logger_provider).with_filter(otel_log_filter);
 
     let tokio_console_layer = console_subscriber::spawn();
 
@@ -53,21 +122,46 @@ fn init_logger() -> SdkLoggerProvider {
         .with_thread_names(true)
         .with_filter(stdout_filter);
 
+    let tracer = tracer_provider.tracer("tracing-otel-subscriber");
     tracing_subscriber::registry()
         .with(tokio_console_layer)
-        .with(otel_layer)
+        .with(otel_log_layer)
         .with(stdout_layer)
+        .with(tracing_opentelemetry::MetricsLayer::new(
+            meter_provider.clone(),
+        ))
+        .with(tracing_opentelemetry::OpenTelemetryLayer::new(tracer))
         .init();
+    OtelGuard {
+        tracer_provider,
+        meter_provider,
+        logger_provider,
+    }
+}
+
+fn init_logger() -> SdkLoggerProvider {
+    let otlp_log_exporter = LogExporter::builder().with_tonic().build().unwrap();
+    let logger_provider = SdkLoggerProvider::builder()
+        .with_resource(RESOURCE.clone())
+        .with_batch_exporter(otlp_log_exporter)
+        .build();
     logger_provider
 }
 
-fn init_metrics() -> SdkMeterProvider {
-    let otlp_metric_exporter = MetricExporter::builder().with_tonic().build().unwrap();
+fn init_meter() -> SdkMeterProvider {
+    let otlp_metric_exporter = MetricExporter::builder()
+        .with_tonic()
+        .with_temporality(opentelemetry_sdk::metrics::Temporality::default())
+        .build()
+        .unwrap();
+
+    let periodic_reader = PeriodicReader::builder(otlp_metric_exporter)
+        .with_interval(std::time::Duration::from_secs(5))
+        .build();
 
     let meter_provider = SdkMeterProvider::builder()
-        // .with_periodic_exporter(exporter)
-        .with_periodic_exporter(otlp_metric_exporter)
         .with_resource(RESOURCE.clone())
+        .with_reader(periodic_reader)
         .build();
     global::set_meter_provider(meter_provider.clone());
 
@@ -75,14 +169,43 @@ fn init_metrics() -> SdkMeterProvider {
 }
 
 fn init_tracer() -> SdkTracerProvider {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
     let otlp_span_exporter = SpanExporter::builder().with_tonic().build().unwrap();
 
     let tracer_provider = SdkTracerProvider::builder()
-        .with_simple_exporter(otlp_span_exporter)
+        .with_resource(RESOURCE.clone())
+        .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+            1.0,
+        ))))
+        .with_id_generator(RandomIdGenerator::default())
+        .with_batch_exporter(otlp_span_exporter)
+        // .with_simple_exporter(otlp_span_exporter)
         .build();
     global::set_tracer_provider(tracer_provider.clone());
 
     tracer_provider
+}
+
+struct OtelGuard {
+    tracer_provider: SdkTracerProvider,
+    meter_provider: SdkMeterProvider,
+    logger_provider: SdkLoggerProvider,
+}
+
+impl Drop for OtelGuard {
+    fn drop(&mut self) {
+        eprintln!("Shutting down observability");
+        if let Err(err) = self.tracer_provider.shutdown() {
+            eprintln!("{err:?}");
+        }
+        if let Err(err) = self.meter_provider.shutdown() {
+            eprintln!("{err:?}");
+        }
+        if let Err(err) = self.logger_provider.shutdown() {
+            eprintln!("{err:?}");
+        }
+    }
 }
 
 /// Entry point for the application
@@ -131,8 +254,40 @@ fn main() {
 
 /// Asynchronous main function
 async fn application_main(args: Args) -> anyhow::Result<()> {
+    let otel_guard = Arc::new(init_observability());
+
+    let handler_otel_guard = otel_guard.clone();
+    tokio::spawn(async move {
+        ctrl_c().await.unwrap();
+        eprintln!("Preparing for unclean exit");
+
+        handler_otel_guard.logger_provider.shutdown().unwrap();
+        handler_otel_guard.meter_provider.shutdown().unwrap();
+        handler_otel_guard.tracer_provider.shutdown().unwrap();
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        eprintln!("Exiting");
+        exit(1);
+    });
+
     // Start exporting system metrics
-    tokio::task::spawn_blocking(export_system_metrics);
+    tokio::task::spawn(export_system_metrics());
+
+    // // Create a tracing layer with the configured tracer
+    // let telemetry =
+    //     tracing_opentelemetry::layer().with_tracer(otel_guard.tracer_provider.tracer("testingsss"));
+
+    // // Use the tracing subscriber `Registry`, or any other subscriber
+    // // that impls `LookupSpan`
+    // let subscriber = Registry::default().with(telemetry);
+    // // Trace executed code
+    // tracing::subscriber::with_default(subscriber, || {
+    //     // Spans will be sent to the configured OpenTelemetry exporter
+    //     let root = span!(tracing::Level::TRACE, "app_start", work_units = 2);
+    //     let _enter = root.enter();
+
+    //     error!("This event will be logged in the root span.");
+    // });
 
     // connect to the database
     let db = database::connect(args.db, &args.username, &args.password)
@@ -171,20 +326,28 @@ async fn application_main(args: Args) -> anyhow::Result<()> {
 
     let db_clone = db.clone();
     tokio::spawn(async move {
-        let mut last_count = 0;
+        let indexed_repos_counter = global::meter("indexer")
+            .u64_counter("indexer.repos.indexed")
+            .with_description("Total number of indexed repos")
+            .with_unit("{repo}")
+            .build();
+        let mut last_count: u64 = 0;
+        let mut interval = interval_at(
+            tokio::time::Instant::now(),
+            tokio::time::Duration::from_secs(2),
+        );
         loop {
             let mut res = db_clone
                 .query("SELECT count() as c FROM li_did GROUP ALL;")
                 .await
                 .unwrap();
             let count: Option<i64> = res.take((0, "c")).unwrap();
-            info!(
-                "fully indexed repo count: {} with {} repos/10s",
-                count.unwrap_or(0),
-                count.unwrap_or(0) - last_count
-            );
-            last_count = count.unwrap_or(0);
-            tokio::time::sleep(std::time::Duration::from_millis(10000)).await;
+            let count = count.unwrap_or(last_count as i64) as u64;
+
+            indexed_repos_counter.add(count - last_count, &[]);
+            last_count = count;
+
+            interval.tick().await;
         }
     });
 
@@ -195,11 +358,6 @@ async fn application_main(args: Args) -> anyhow::Result<()> {
             tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
         }
     }
-
-    // TODO: Also handle shutdown when an error occurs
-    tracer_provider.shutdown().unwrap();
-    metrics_provider.shutdown().unwrap();
-    logger_provider.shutdown().unwrap();
 
     Ok(())
 }
