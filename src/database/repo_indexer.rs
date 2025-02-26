7f602ceb65a80265061e6141de486d06ba3d4f43
Zebreus <zebreus@zebre.us>
2025-02-26 16:45:46 +0100

    Add more pipeline metrics

--- src/database/repo_indexer.rs
+++ src/database/repo_indexer.rs
@@ -1,6 +1,6 @@
 use futures::StreamExt;
 use index_repo::PipelineItem;
-use opentelemetry::global;
+use opentelemetry::{global, KeyValue};
 use repo_stream::RepoStream;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -34,6 +34,72 @@ const BUFFER_SIZE: usize = 30;
 /// Buffer size multiplier for the download stage
 const DOWNLOAD_BUFFER_SIZE: usize = 6;
 
+// Make this less hacky
+macro_rules! stage {
+    ($metric:ident, $stage:literal, $next:literal, $item:ident -> $content:expr) => {
+        |$item| async {
+            $metric.add(
+                -1,
+                &[
+                    KeyValue::new("stage", $stage),
+                    KeyValue::new("state", "queued"),
+                ],
+            );
+            $metric.add(
+                1,
+                &[
+                    KeyValue::new("stage", $stage),
+                    KeyValue::new("state", "active"),
+                ],
+            );
+
+            let result = $content;
+
+            $metric.add(
+                -1,
+                &[
+                    KeyValue::new("stage", $stage),
+                    KeyValue::new("state", "active"),
+                ],
+            );
+            $metric.add(
+                1,
+                &[
+                    KeyValue::new("stage", $next),
+                    KeyValue::new("state", "queued"),
+                ],
+            );
+
+            result
+        }
+    };
+}
+
+// Make this less hacky
+macro_rules! filter_result {
+    ($metric:ident, $stage:literal) => {|result| async {
+        if let Err(error) = &result {
+            error!(target: "indexer", "Failed to index repo: {}", error);
+            $metric.add(
+                -1,
+                &[
+                    KeyValue::new("stage", $stage),
+                    KeyValue::new("state", "queued"),
+                ],
+            );
+            return None;
+        }
+        result.ok()
+    }};
+}
+
+// async fn filter_result<T>(result: anyhow::Result<T>) -> Option<T> {
+//     if let Err(error) = &result {
+//         error!(target: "indexer", "Failed to index repo: {}", error);
+//     }
+//     result.ok()
+// }
+
 pub async fn start_full_repo_indexer(db: Surreal<Any>) -> anyhow::Result<()> {
     let http_client = Client::new();
 
@@ -41,6 +107,11 @@ pub async fn start_full_repo_indexer(db: Surreal<Any>) -> anyhow::Result<()> {
     let repos_indexed = meter
         .u64_counter("indexer.repos.indexed")
         .with_description("Total number of indexed repos")
+        .with_unit("repo")
+        .build();
+    let tracker = meter
+        .i64_up_down_counter("indexer.pipeline.location")
+        .with_description("Track the number of tasks in the pipeline")
         .with_unit("repo")
         .build();
 
@@ -55,64 +126,74 @@ pub async fn start_full_repo_indexer(db: Surreal<Any>) -> anyhow::Result<()> {
     repos_indexed.add(count as u64, &[]);
 
     RepoStream::new(OLDEST_USEFUL_ANCHOR.to_string(), &db)
-        .map(|did| async { did })
-        .buffer_unordered(BUFFER_SIZE)
-        .map(|did| {
+        .map(|did| async {
             let db = &db;
             let http_client = &http_client;
             let item = PipelineItem::new(db, http_client, did);
+
+            tracker.add(
+                1,
+                &[
+                    KeyValue::new("stage", "check_indexed"),
+                    KeyValue::new("state", "queued"),
+                ],
+            );
             item
         })
-        .map(|item| async { item.check_indexed().await })
         .buffer_unordered(BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
-        .map(|item| async { item.get_service().await })
+        .map(stage!(tracker, "check_indexed", "get_service", item ->
+            item.check_indexed().await
+        ))
         .buffer_unordered(BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
-        .map(|item| async { item.download_repo().await })
+        .filter_map(filter_result!(tracker, "get_service"))
+        .map(stage!(tracker, "get_service", "download_repo", item ->
+            item.get_service().await
+        ))
+        .buffer_unordered(BUFFER_SIZE)
+        .filter_map(filter_result!(tracker, "download_repo"))
+        .map(stage!(tracker, "download_repo", "deserialize_repo", item ->
+            item.download_repo().await
+        ))
         .buffer_unordered(BUFFER_SIZE * DOWNLOAD_BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
-        .map(|item| async { item.deserialize_repo().await })
+        .filter_map(filter_result!(tracker, "deserialize_repo"))
+        .map(stage!(tracker, "deserialize_repo", "parse_repo", item ->
+            item.deserialize_repo().await
+        ))
         .buffer_unordered(BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
-        .map(|item| async { item.files_to_updates().await })
+        .filter_map(filter_result!(tracker, "files_to_updates"))
+        .map(stage!(tracker, "files_to_updates", "apply_updates", item ->
+            item.files_to_updates().await
+        ))
         .buffer_unordered(BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
-        .map(|item| async { item.apply_updates().await })
+        .filter_map(filter_result!(tracker, "apply_updates"))
+        .map(stage!(tracker, "apply_updates", "print_report", item ->
+            item.apply_updates().await
+        ))
         .buffer_unordered(BUFFER_SIZE)
-        .filter_map(|result| async {
-            if let Err(error) = &result {
-                error!(target: "indexer", "Failed to index repo: {}", error);
-            }
-            result.ok()
-        })
+        .filter_map(filter_result!(tracker, "print_report"))
         .for_each(|x| async {
+            tracker.add(
+                -1,
+                &[
+                    KeyValue::new("stage", "print_report"),
+                    KeyValue::new("state", "queued"),
+                ],
+            );
+            tracker.add(
+                1,
+                &[
+                    KeyValue::new("stage", "print_report"),
+                    KeyValue::new("state", "active"),
+                ],
+            );
             x.print_report().await;
+            tracker.add(
+                -1,
+                &[
+                    KeyValue::new("stage", "print_report"),
+                    KeyValue::new("state", "active"),
+                ],
+            );
             repos_indexed.add(1, &[]);
         })
         .await;
