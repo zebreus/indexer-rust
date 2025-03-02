18de229e9d344a4f1f8b5e884c08f29272d313c8
Zebreus <zebreus@zebre.us>
2025-03-02 22:40:45 +0100

    Improve server setup script

--- setup.sh
+++ setup.sh
@@ -12,9 +12,16 @@ if ! git status; then
     git clone https://github.com/zebreus/indexer-rust
     cd indexer-rust
 fi
+cargo install samply
+echo '1' >/proc/sys/kernel/perf_event_paranoid
+echo '-1' >/proc/sys/kernel/perf_event_paranoid
+
+wget https://github.com/zebreus/upload/releases/download/v0.2/upload.binary
+chmod +x upload.binary
+mv upload.binary /usr/local/bin/upload
 
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://monitoring.indexer.skyfeedlol.lol:39291"
 echo 'export OTEL_EXPORTER_OTLP_ENDPOINT="http://monitoring.indexer.skyfeedlol.lol:39291"' >~/.bashrc
 
-cargo build
-echo 'Done! Run `./target/debug/indexer-rust --db "rocksdb:///root/rocks/db" --mode full` to start the indexer.'
+cargo build --release
+echo 'Done! Run `samply record ./target/release/indexer --db "rocksdb:///root/rocks/db" --mode full` to start the indexer.'
